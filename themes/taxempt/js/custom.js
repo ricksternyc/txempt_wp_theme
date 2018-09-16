a20116ebd64d7555f55b347369dc8adc0c5cc762
Rick Dev <rickthewebdev@gmail.com>
2018-09-16 18:16:33 -0400

    mobile bugs

--- themes/taxempt/js/custom.js
+++ themes/taxempt/js/custom.js
@@ -1,14 +1,14 @@
 
 $(function() {//DOM ready
 
-  $('.carousel').carousel({
-      interval: 2000
+    $('.carousel').carousel({
+        interval: 2000
     })
-    
-    
 
 
-//CARD BOUNCE
+
+
+    //CARD BOUNCE
     $('.card_bounce').each( function(i){
 
         /*change position of each card*/
@@ -43,10 +43,25 @@ $(function() {//DOM ready
                     .animate({'marginTop':'20px'},{queue:false,duration: 1600, easing:"easeOutElastic"})}      
 
         }); 
-
     });
 
 
-
-
+    if ($('body').hasClass('mobile')) {
+        var viewportHeight = $('.carousel-item').innerHeight();
+        $('.carousel-item').css({ height: viewportHeight });
+    }
 });
+
+
+//CHROME VH FIX
+
+var VHChromeFix=function(e){var t=this,i=navigator.userAgent.toLowerCase(),n=/chrome/.test(i)&&/android/.test(i),o=/crios/.test(i);(n||o)&&(this.getElements(e),this.fixAll(),this.windowWidth=window.innerWidth,this.windowHeight=window.innerHeight,window.addEventListener("resize",function(){t.windowWidth!==window.innerWidth&&t.windowHeight!==window.innerHeight&&(t.windowWidth=window.innerWidth,t.windowHeight=window.innerHeight,t.fixAll())}))};VHChromeFix.prototype.getElements=function(e){this.elements=[],e=this.isArray(e)?e:[e];for(var t=0;t<e.length;t++)for(var i=e[t].selector,n=document.querySelectorAll(i),o=0;o<n.length;o++)this.elements.push({domElement:n[o],vh:e[t].vh})},VHChromeFix.prototype.isArray=function(e){return"[object Array]"===Object.prototype.toString.call(e)},VHChromeFix.prototype.fixAll=function(){for(var e=0;e<this.elements.length;e++){var t=this.elements[e];t.domElement.style.height=window.innerHeight*t.vh/100+"px"}};
+
+var options = [
+  {
+    selector: '.carousel-item', // Mandatory, CSS selector
+    vh: 110,  // Mandatory, height in vh units
+  }
+];
+
+var vhFix = new VHChromeFix(options);
